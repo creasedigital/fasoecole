4e4028313b6af5080f80bc96e0521003eb539f1d
Ojore Chris <ojorechris@gmail.com>
2023-01-30 17:23:08 +0100

    default route to signup

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,7 @@ const App = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/signup" element={<Signup />} />
+				<Route path="/" element={<Signup />} />
 				<Route path="/verify-email" element={<VerifyEmail />} />
 			</Routes>
 		</BrowserRouter>
